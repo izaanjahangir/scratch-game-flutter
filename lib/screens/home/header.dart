cd3f334116414e210a85f491c5288649ed2ddb5b
izaanjahangir <izaanjahangir2@gmail.com>
2021-03-26 21:42:41 +0500

    added basic header

---
+++ lib/screens/home/header.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      height: height * 0.075,
+      color: Color.fromRGBO(0, 0, 0, 0.8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [ChipButton(label: "Easy")],
+      ),
+    );
+  }
+}
+
+class ChipButton extends StatelessWidget {
+  final String label;
+
+  ChipButton({this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(15)),
+      child: Material(
+        borderRadius: BorderRadius.circular(15),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: () {},
+          child: Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: height * 0.02, vertical: height * 0.005),
+            child: Row(
+              children: [
+                Text(
+                  label,
+                  style: TextStyle(fontSize: 14),
+                ),
+                Icon(
+                  Icons.arrow_drop_down,
+                  size: height * 0.03,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import "package:scratcher_game/components/fruit/fruit.dart";
+import "package:scratcher_game/screens/home/header.dart";
 
 class Home extends StatelessWidget {
   @override
@@ -7,58 +8,56 @@ class Home extends StatelessWidget {
     final double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(bottom: 15, left: 15, right: 15),
-        // child: GridView.count(
-        //   crossAxisCount: 2,
-        //   children: List.generate(4, (index) {
-        //     return Fruit();
-        //   }),
-        // ),
+      body: SafeArea(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Header(),
             Container(
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.only(bottom: 15, left: 15, right: 15),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "An Apple a day keeps a doctor away",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18),
+                  Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      children: [
+                        Text(
+                          "An Apple a day keeps a doctor away",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Find the apple",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.red,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
                   SizedBox(
-                    height: 5,
+                    height: 10,
                   ),
-                  Text(
-                    "Find the apple",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 24,
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold),
+                  GridView.count(
+                    physics: new NeverScrollableScrollPhysics(),
+                    mainAxisSpacing: 5,
+                    crossAxisSpacing: 5,
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    children: List.generate(4, (index) {
+                      return Fruit(
+                        width: width,
+                      );
+                    }),
                   ),
                 ],
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            GridView.count(
-              physics: new NeverScrollableScrollPhysics(),
-              mainAxisSpacing: 5,
-              crossAxisSpacing: 5,
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              children: List.generate(4, (index) {
-                return Fruit(
-                  width: width,
-                );
-              }),
             ),
           ],
         ),
